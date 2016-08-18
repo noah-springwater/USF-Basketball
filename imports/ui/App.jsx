7333afbc13b283894915c85779a3ddf95646c8d1
Noah Springwater <nspringwater@gmail.com>
2016-08-18 18:10:16 -0400

    add/delete player

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,33 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
 
-import Player from './Player.jsx'
+import { Players } from '../api/players.js';
 
-export default class App extends Component {
-  getPlayers() {
-    return [
-      { _id: 1, name: 'Noah' },
-      { _id: 2, name: 'John' },
-      { _id: 3, name: 'Deshawn' },
-    ];
+import Player from './Player.jsx';
+
+class App extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // Find the text field via the React ref
+    const name = ReactDOM.findDOMNode(this.refs.textInput).value.trim().toUpperCase();
+    console.log(name.toUpperCase());
+
+    Players.insert({
+      name, //name variable from above
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
   renderPlayers() {
-     return this.getPlayers().map((player) => (
+     return this.props.players.map((player) => (
        <Player key={player._id} player={player} />
      ));
+
    }
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Test</h1>
         </header>
-
-        <ul>
-          {this.renderPlayers()}
-        </ul>
+        <div className="menu">
+          <ul>
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+               <input type="text" ref="textInput" placeholder="Player Name" />
+            </form>
+            {this.renderPlayers()}
+          </ul>
+      </div>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  players: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    players: Players.find({}, { sort: { createdAt: -1 } }).fetch(),
+  }
+}, App);
--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -1,11 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Players } from '../api/players.js';
+
 // Task component - represents a single todo item
 export default class Player extends Component {
+  deletePlayer() {
+    Players.remove(this.props.player._id);
+  }
+
   render() {
     return (
-      <li>{this.props.player.name}</li>
-    );
+      <div className="hello">
+        <li>{this.props.player.name}</li>
+        <button className="delete" onClick={this.deletePlayer.bind(this)}>&times;</button>
+      </div>
+  );
   }
 }
 
